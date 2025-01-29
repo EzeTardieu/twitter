15c46662df3de0adb1323025f32b9df532c2d94d
Ezequiel Tardieu <ezequieltardieu@gmail.com>
2025-01-29 01:22:14 -0300

    add swagger

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace Web;
 
 public class Program
@@ -10,15 +12,19 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        });
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseAuthorization();
